971bc8ab019b72309e5e01861d5e7c4705becf20
weEQxuw <dondon666666@mail.ru>
2024-09-23 00:24:38 +0400

    new version

--- include/set/set.h
+++ include/set/set.h
@@ -40,6 +40,8 @@ namespace set
 		}
 	}
 
+	
+
 
 	class Set
 	{
@@ -49,7 +51,7 @@ namespace set
 
 	public: 
 
-		Node* get_root()
+		Node* get_root() const
 		{
 			return _root;
 		}
@@ -61,9 +63,30 @@ namespace set
 
 		Set() : _root(nullptr) {};
 
-		Node* copy_set(Node* ptr) 
+		Set(const Set& another)
+		{
+			_root = copy_set(another._root);
+		}
+
+		void delete_node(Node*& ptr)
 		{
 			if (ptr == nullptr)
+				return;
+
+			delete_node(ptr->_left);
+			delete_node(ptr->_right);
+			delete ptr;
+			ptr = nullptr;
+		}
+
+		~Set()
+		{
+			delete_node(_root);
+		}
+
+		Node* copy_set(Node* ptr) 
+		{
+			if (!ptr)
 				return nullptr;
 			
 			Node* new_node = new Node(ptr->_value);
@@ -73,33 +96,204 @@ namespace set
 
 		}
 
-		Set(Set& another) 
-		{
-			_root = copy_set(another._root);
-		}
-
 		void print_set()
 		{
 			return print(_root);
 		}
 
-		bool contains(int key)
+		bool contains(int key) const
 		{
 			Node* ptr = _root;
 
-			while (ptr != nullptr || ptr->_value == key)
+			while (ptr != nullptr && ptr->_value == key)
 			{
 				if (key < ptr->_value)
 					ptr = ptr->_left;
 				else
 					ptr = ptr->_right;
-
-
 			}
+
 			return ptr != nullptr;
 
 		}
 
+		bool insert(int value)
+		{
+			if (_root == nullptr)
+			{
+				_root = new Node(value);
+				return true;
+			}
+
+			if (contains(value) == true)
+				return false;
+
+			Node* ptr = _root;
+
+			while (ptr != nullptr)
+			{
+				if (value < ptr->_value && ptr->_left == nullptr)
+				{
+					ptr->_left = new Node(value);
+					return true;
+				}
+
+				if (value > ptr->_value && ptr->_right == nullptr)
+				{
+					ptr->_right = new Node(value);
+					return true;
+				}
+
+				if (value < ptr->_value)
+					ptr = ptr->_left;
+				else
+					ptr = ptr->_right;
+			}
+		}
+
+		bool erase(int value)
+		{
+
+			Node* ptr = _root;
+			Node* pre_ptr = nullptr;
+
+			while (ptr && ptr->_value != value)
+			{
+				pre_ptr = ptr;
+
+				if (value < ptr->_value)
+					ptr = ptr->_left;
+				else
+					ptr = ptr->_right;
+
+				
+			}
+
+			if (ptr == nullptr)
+				return false;
+
+			if (ptr->_left == nullptr && ptr->_right == nullptr)
+			{
+				if (ptr != _root)
+				{
+					if (pre_ptr->_left == ptr)
+						pre_ptr->_left == nullptr;
+					else
+						pre_ptr->_right == nullptr;
+				}
+				else
+					_root = nullptr;
+
+				delete ptr;
+				return true;
+
+			}
+
+			if (ptr->_left == nullptr)
+			{
+				if (ptr == _root)
+				{
+					_root = _root->_right;
+					delete ptr;
+					return true;
+				}
+
+				if (pre_ptr->_left == ptr)
+				{
+					pre_ptr->_left = ptr->_right;
+				}
+
+				if (pre_ptr->_right == ptr)
+				{
+					pre_ptr->_right == ptr->_right;
+				}
+
+				delete ptr;
+				return true;
+			}
+
+			if (ptr->_right == nullptr)
+			{
+				if (ptr == _root)
+				{
+					_root = _root->_left;
+					delete ptr;
+					return true;
+				}
+
+				if (pre_ptr->_left == ptr)
+				{
+					pre_ptr->_left = ptr->_left;
+				}
+
+				if (pre_ptr->_right == ptr)
+				{
+					pre_ptr->_right = ptr->_left;
+				}
+
+				delete ptr;
+				return true;
+
+			}
+
+			Node* extra_ptr = ptr->_right;
+			pre_ptr = ptr;
+
+			while (extra_ptr->_left)
+			{
+				pre_ptr = extra_ptr;
+				extra_ptr = extra_ptr->_left;
+			}
+
+			ptr->_value = extra_ptr->_value;
+
+			if (pre_ptr == ptr)
+				ptr->_right = extra_ptr->_right;
+			else
+				pre_ptr->_left = extra_ptr->_right;
+
+			delete extra_ptr;
+		}
+
 	};
+
+	void sub_union(Node* ptr, Set& c) {
+		if (ptr == nullptr) {
+			return;
+		}
+		c.insert(ptr->_value);
+		sub_union(ptr->_left, c);
+		sub_union(ptr->_right, c);
+	};
+
+	Set uni(const Set& a, const Set& b) {
+		Set c(b);
+		sub_union(a.get_root(), c);
+		return c;
+	}
+
+	void sub_symmetrical_difference(Node* ptr, const Set& b, Set& c) {
+		if (ptr == nullptr) {
+			return;
+		}
+		if (!b.contains(ptr->_value)) {
+			c.insert(ptr->_value);
+		}
+		sub_symmetrical_difference(ptr->_left, b, c);
+		sub_symmetrical_difference(ptr->_right, b, c);
+	};
+
+	Set symmetrical_difference(const Set& a, const Set& b) {
+		Set c;
+		sub_symmetrical_difference(a.get_root(), b, c);
+		sub_symmetrical_difference(b.get_root(), a, c);
+		return c;
+	}
+
+	size_t lcg() {
+		static size_t x = 0;
+		x = (1021 * x + 24631) % 116640;
+		return x;
+	}
 
 }
---
+++ test/set_tests.cc
@@ -0,0 +1,198 @@
+#include <set/set.h>
+#include <random>
+#include <chrono>
+#include <vector>
+
+using namespace set;
+
+int main() {
+
+	setlocale(LC_CTYPE, "");
+
+	int sum = 0;
+	for (int j = 0; j < 100; ++j) {
+		Set a;
+		auto start = chrono::steady_clock::now();
+		for (int i = 0; i < 1000; ++i) {
+			a.insert(lcg());
+		}
+		auto end = chrono::steady_clock::now();
+		sum = sum + chrono::duration_cast<chrono::microseconds>(end - start).count();
+	}
+	cout << 1.0 * sum / 100 << " микросекунд - среднее время заполнения множества 1000 элементами" << endl;
+
+	sum = 0;
+	for (int j = 0; j < 100; ++j) {
+		Set a;
+		auto start = chrono::steady_clock::now();
+		for (int i = 0; i < 10000; ++i) {
+			a.insert(lcg());
+		}
+		auto end = chrono::steady_clock::now();
+		sum = sum + chrono::duration_cast<chrono::microseconds>(end - start).count();
+	}
+	cout << 1.0 * sum / 100 << " микросекунд - среднее время заполнения множества 10000 элементами" << endl;
+	sum = 0;
+	for (int j = 0; j < 100; ++j) {
+		Set a;
+		auto start = chrono::steady_clock::now();
+		for (int i = 0; i < 100000; ++i) {
+			a.insert(lcg());
+		}
+		auto end = chrono::steady_clock::now();
+		sum = sum + chrono::duration_cast<chrono::microseconds>(end - start).count();
+	}
+	cout << 1.0 * sum / 100 << " микросекунд - среднее время заполнения множества 100000 элементами" << endl;
+
+	cout << "Сравнение\tУдаление\tДобавление" << endl;
+
+	Set a;
+
+	for (int i = 0; i < 1000; ++i) {
+		a.insert(lcg());
+	}
+
+	int sum_contains = 0, sum_erase = 0, sum_insert = 0;
+	for (int j = 0; j < 1000; ++j) {
+		Set b(a);
+		auto start = chrono::steady_clock::now();
+		b.contains(lcg());
+		auto end = chrono::steady_clock::now();
+		sum_contains = sum_contains + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		b.erase(lcg());
+		end = chrono::steady_clock::now();
+		sum_erase = sum_erase + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		b.insert(lcg());
+		end = chrono::steady_clock::now();
+		sum_insert = sum_insert + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+	}
+	cout << 1.0 * sum_contains / 1000 << "\t" << 1.0 * sum_erase / 1000 << "\t" << 1.0 * sum_insert / 1000 << "\t" << endl;
+
+
+	for (int i = 0; i < 10000; ++i) {
+		a.insert(lcg());
+	}
+
+	sum_contains = 0, sum_erase = 0, sum_insert = 0;
+	for (int j = 0; j < 1000; ++j) {
+		Set b(a);
+		auto start = chrono::steady_clock::now();
+		b.contains(lcg());
+		auto end = chrono::steady_clock::now();
+		sum_contains = sum_contains + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		b.erase(lcg());
+		end = chrono::steady_clock::now();
+		sum_erase = sum_erase + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		b.insert(lcg());
+		end = chrono::steady_clock::now();
+		sum_insert = sum_insert + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+	}
+	cout << 1.0 * sum_contains / 1000 << "\t" << 1.0 * sum_erase / 1000 << "\t" << 1.0 * sum_insert / 1000 << "\t" << endl;
+
+
+	for (int i = 0; i < 100000; ++i) {
+		a.insert(lcg());
+	}
+
+	sum_contains = 0, sum_erase = 0, sum_insert = 0;
+	for (int j = 0; j < 1000; ++j) {
+		Set b(a);
+		auto start = chrono::steady_clock::now();
+		b.contains(lcg());
+		auto end = chrono::steady_clock::now();
+		sum_contains = sum_contains + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		b.erase(lcg());
+		end = chrono::steady_clock::now();
+		sum_erase = sum_erase + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		b.insert(lcg());
+		end = chrono::steady_clock::now();
+		sum_insert = sum_insert + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+	}
+	cout << 1.0 * sum_contains / 1000 << "\t" << 1.0 * sum_erase / 1000 << "\t" << 1.0 * sum_insert / 1000 << "\t" << endl;
+
+
+/*
+	Set k1, k2;
+	for (int i = 0; i < 10; ++i) {
+		k1.insert(rand() % 10);
+		k2.insert(rand() % 10);
+	}
+	k1.print_set();
+	cout << endl;
+	k2.print_set();
+	cout << endl;
+	uni(k1, k2).print_set();
+	cout << endl;
+	symmetrical_difference(k1, k2).print_set();
+
+	*/
+	/*int sum = 0;
+	for (int j = 0; j < 100; ++j) {
+		vector<int> a;
+		auto start = chrono::steady_clock::now();
+		for (int i = 0; i < 1000; ++i) {
+			a.push_back(lcg());
+		}
+		auto end = chrono::steady_clock::now();
+		sum = sum + chrono::duration_cast<chrono::microseconds>(end - start).count();
+	}
+	cout << 1.0 * sum / 100 << " микросекунд - среднее время заполнения вектора 1000 элементами" << endl;
+
+	sum = 0;
+	for (int j = 0; j < 100; ++j) {
+		vector<int> a;
+		auto start = chrono::steady_clock::now();
+		for (int i = 0; i < 10000; ++i) {
+			a.push_back(lcg());
+		}
+		auto end = chrono::steady_clock::now();
+		sum = sum + chrono::duration_cast<chrono::microseconds>(end - start).count();
+	}
+	cout << 1.0 * sum / 100 << " микросекунд - среднее время заполнения вектора 10000 элементами" << endl;
+	sum = 0;
+	for (int j = 0; j < 100; ++j) {
+		vector<int> a;
+		auto start = chrono::steady_clock::now();
+		for (int i = 0; i < 100000; ++i) {
+			a.push_back(lcg());
+		}
+		auto end = chrono::steady_clock::now();
+		sum = sum + chrono::duration_cast<chrono::microseconds>(end - start).count();
+	}
+	cout << 1.0 * sum / 100 << " микросекунд - среднее время заполнения вектора 100000 элементами" << endl;
+
+	cout << "Сравнение\tУдаление\tДобавление" << endl;
+
+	vector<int> a;
+	for (int i = 0; i < 1000; ++i) {
+		a.push_back(lcg());
+	}
+
+	int sum_contains = 0, sum_erase = 0, sum_insert = 0;
+	for (int j = 0; j < 1000; ++j) {
+		int index_contains = a[rand() % 1000], index_remove = rand() % 1000, index_insert = lcg();
+		auto beg = a.cbegin(), end1 = a.cend();
+		auto start = chrono::steady_clock::now();
+		find(beg, end1, index_contains);
+		auto end = chrono::steady_clock::now();
+		sum_contains = sum_contains + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		a.erase(beg+index_remove);
+		end = chrono::steady_clock::now();
+		sum_erase = sum_erase + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		start = chrono::steady_clock::now();
+		a.insert(beg + index_insert, lcg());
+		end = chrono::steady_clock::now();
+		sum_insert = sum_insert + chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+	}
+	cout << 1.0 * sum_contains / 1000 << "\t" << 1.0 * sum_erase / 1000 << "\t" << 1.0 * sum_insert / 1000 << "\t" << endl;*/
+
+
+	return 0;
+}
